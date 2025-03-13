8079e620ea2565798d0d2bc5b065aa2ba64f4245
akash <155128336+Akashks3@users.noreply.github.com>
2025-03-13 12:38:10 +0530

    Update validate.js

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -10,12 +10,12 @@ export const checkValidSignInFrom = (email, password) => {
 	if (!isPasswordValid) return "Invalid password";
 	return null;
 };
-export const checkValidSignUpFrom = (firstName, lastName, email, password) => {
+export const checkValidSignUpFrom = (firstName,email, password) => {
 	const isFirstValid = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(firstName);
 	const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(
 		email
 	);
-	if (!isFirstValid) return "Invalid FirstName Format";
+	if (!isFirstValid) return "Invalid or first letter uppercase FirstName Format";
 	if (!isEmailValid) return "Invalid email format";
 	if (password.length < 8) return "Min 8 characters";
 	if (!/[a-z]/.test(password)) return "Needs password 1 lowercase letter";
